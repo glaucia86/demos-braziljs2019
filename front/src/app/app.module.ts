90d0dab3640f793bacd3229c8f58f5409c8e8b7c
glaucia86 <glaucia_lemos86@hotmail.com>
2019-08-01 17:13:36 -0300

    Inclusão de bibliotecas slimloadingbar

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { PalestranteEditComponent } from './palestrante-edit/palestrante-edit.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
