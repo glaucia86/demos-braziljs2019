076327d909bd657e4d69c742e770ce5530d0ad29
glaucia86 <glaucia_lemos86@hotmail.com>
2019-08-01 16:01:44 -0300

    Criando os componentes e definindo as rotas dos componentes: front.

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PalestranteAddComponent } from './palestrante-add/palestrante-add.component';
+import { PalestranteEditComponent } from './palestrante-edit/palestrante-edit.component';
+import { PalestranteGetComponent } from './palestrante-get/palestrante-get.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'palestrante/create', component: PalestranteAddComponent },
+  { path: 'palestrante/edit/:id', component: PalestranteEditComponent },
+  { path: 'palestrante', component: PalestranteGetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
